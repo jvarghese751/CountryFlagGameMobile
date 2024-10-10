0b2460e7e597eb053e1363b152533143d9e0c1d7
jvarghese751 <varghese-J2@ulster.ac.uk>
2024-10-10 14:15:28 +0100

    new country game

--- app/src/main/java/com/example/countryflaggamemobiletech/MainActivity.java
+++ app/src/main/java/com/example/countryflaggamemobiletech/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.countryflaggamemobiletech;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -43,8 +45,8 @@ public class MainActivity extends AppCompatActivity {
         btnNewCountryGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Placeholder code for New Country Game
-                Toast.makeText(MainActivity.this, "New Country Game Started!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, NewCountryGameActivity.class);
+                startActivity(intent);
             }
         });
     }
---
+++ app/src/main/java/com/example/countryflaggamemobiletech/NewCountryGameActivity.java
@@ -0,0 +1,36 @@
+package com.example.countryflaggamemobiletech;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class NewCountryGameActivity extends AppCompatActivity {
+    private Button btnGuessCountry, btnGuessHints, btnGuessFlag, btnAdvancedLevel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_country_game);
+
+        btnGuessCountry = findViewById(R.id.btnGuessCountry);
+        btnGuessHints = findViewById(R.id.btnGuessHints);
+        btnGuessFlag = findViewById(R.id.btnGuessFlag);
+        btnAdvancedLevel = findViewById(R.id.btnAdvancedLevel);
+
+        btnGuessCountry.setOnClickListener(v -> {
+            Toast.makeText(NewCountryGameActivity.this, "Guess the Country Selected", Toast.LENGTH_SHORT).show();
+        });
+
+        btnGuessHints.setOnClickListener(v -> {
+            Toast.makeText(NewCountryGameActivity.this, "Guess-Hints Selected", Toast.LENGTH_SHORT).show();
+        });
+
+        btnGuessFlag.setOnClickListener(v -> {
+            Toast.makeText(NewCountryGameActivity.this, "Guess the Flag Selected", Toast.LENGTH_SHORT).show();
+        });
+
+        btnAdvancedLevel.setOnClickListener(v -> {
+            Toast.makeText(NewCountryGameActivity.this, "Advanced Level Selected", Toast.LENGTH_SHORT).show();
+        });
+    }
+}
